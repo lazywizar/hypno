b95886eab9bed3f56d125eaf1af15572a559c841
varun kumar <bit.varun@gmail.com>
2014-06-22 21:20:34 -0700

    Initial Commit

---
+++ Hypnosister/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  Hypnosister
+//
+//  Created by varun kumar on 22/06/14.
+//  Copyright (c) 2014 ThinkLazy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by varun kumar on 22/06/14.
+//  Copyright (c) 2014 ThinkLazy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
